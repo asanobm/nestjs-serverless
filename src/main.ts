29cb778f92e96d4b0f7350b4b17673e7970da42d
Asanobm <asanobm@outlook.com>
2022-10-10 10:21:52 +0900

    When NODE_ENV is in production, the application runs serverless.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Server } from 'http';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { createServer, proxy } from 'aws-serverless-express';
+import { eventContext } from 'aws-serverless-express/middleware';
+import { Context, Handler } from 'aws-lambda';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+
+const binaryMimeTypes: string[] = [];
+
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.use(eventContext());
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
-bootstrap();
+
+if (process.env.NODE_ENV !== 'production') {
+  bootstrap().then(() => console.log('Server started'));
+}
